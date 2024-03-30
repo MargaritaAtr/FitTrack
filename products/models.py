143f1611c26ed7ae78a0bc2a27a72beb83391a38
Alex Atr <margokom87@gmail.com>
2024-03-30 22:07:50 +0000

    Added products app, models and fixtures

--- products/models.py
+++ products/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Category (models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def _str_(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField() 
+    new = models.BooleanField(default=False, null=False, blank=False)
+    on_sale = models.BooleanField(default=False, null=False, blank=False)
+    details = models.CharField(max_length=254)
+    materials = models.CharField(max_length=254, null=True,default=None)
+    fabric_care = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    size = models.CharField(max_length=254, null=True, blank=True)
+    weight =  models.CharField(max_length=254, null=True, blank=True)
+    special_price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True, default=None)
+
+    def _str_(self):
+        return self.name
