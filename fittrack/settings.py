777b2c02302485fc114bbf3b51e63de2e2d8511e
Alex Atr <margokom87@gmail.com>
2024-04-22 09:59:56 +0100

    changed Debug in settings

--- fittrack/settings.py
+++ fittrack/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fit-track-0314c0bbcc52.herokuapp.com', '127.0.0.1']
 
